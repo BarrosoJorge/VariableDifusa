0bd0f1352c6ee2a2aadc0382011e6211e7103305
JoseAdrianRodriguezGonzalez <kerpador@gmail.com>
2025-09-29 05:00:43 -0600

    add the variable

---
+++ build/CMakeFiles/VariableDifusa.dir/DependInfo.cmake
@@ -0,0 +1,28 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/adrian/Documents/Uni/Quinto/sistemas/tarea3/VariableDifusa/src/ConjuntoDifuso.cpp" "CMakeFiles/VariableDifusa.dir/src/ConjuntoDifuso.cpp.o" "gcc" "CMakeFiles/VariableDifusa.dir/src/ConjuntoDifuso.cpp.o.d"
+  "/home/adrian/Documents/Uni/Quinto/sistemas/tarea3/VariableDifusa/src/Trapezoidal.cpp" "CMakeFiles/VariableDifusa.dir/src/Trapezoidal.cpp.o" "gcc" "CMakeFiles/VariableDifusa.dir/src/Trapezoidal.cpp.o.d"
+  "/home/adrian/Documents/Uni/Quinto/sistemas/tarea3/VariableDifusa/src/Triangular.cpp" "CMakeFiles/VariableDifusa.dir/src/Triangular.cpp.o" "gcc" "CMakeFiles/VariableDifusa.dir/src/Triangular.cpp.o.d"
+  "/home/adrian/Documents/Uni/Quinto/sistemas/tarea3/VariableDifusa/src/VariableDifusa.cpp" "CMakeFiles/VariableDifusa.dir/src/VariableDifusa.cpp.o" "gcc" "CMakeFiles/VariableDifusa.dir/src/VariableDifusa.cpp.o.d"
+  "/home/adrian/Documents/Uni/Quinto/sistemas/tarea3/VariableDifusa/src/main.cpp" "CMakeFiles/VariableDifusa.dir/src/main.cpp.o" "gcc" "CMakeFiles/VariableDifusa.dir/src/main.cpp.o.d"
+  "" "VariableDifusa" "gcc" "CMakeFiles/VariableDifusa.dir/link.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/CMakeFiles/VariableDifusa.dir/cmake_clean.cmake
@@ -0,0 +1,20 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/VariableDifusa.dir/link.d"
+  "CMakeFiles/VariableDifusa.dir/src/ConjuntoDifuso.cpp.o"
+  "CMakeFiles/VariableDifusa.dir/src/ConjuntoDifuso.cpp.o.d"
+  "CMakeFiles/VariableDifusa.dir/src/Trapezoidal.cpp.o"
+  "CMakeFiles/VariableDifusa.dir/src/Trapezoidal.cpp.o.d"
+  "CMakeFiles/VariableDifusa.dir/src/Triangular.cpp.o"
+  "CMakeFiles/VariableDifusa.dir/src/Triangular.cpp.o.d"
+  "CMakeFiles/VariableDifusa.dir/src/VariableDifusa.cpp.o"
+  "CMakeFiles/VariableDifusa.dir/src/VariableDifusa.cpp.o.d"
+  "CMakeFiles/VariableDifusa.dir/src/main.cpp.o"
+  "CMakeFiles/VariableDifusa.dir/src/main.cpp.o.d"
+  "VariableDifusa"
+  "VariableDifusa.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/VariableDifusa.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
